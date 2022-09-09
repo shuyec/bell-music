2d43034dcb97c9e2cb02b6c15aadf70eba7bf930
Shu <76391491+shuyec@users.noreply.github.com>
2022-09-09 13:52:35 +0200

    Moved out getMedia function

--- lib/screens/media/audio_metadata.dart
+++ lib/screens/media/audio_metadata.dart
@@ -1,11 +1,10 @@
-import 'package:dio/dio.dart';
-
 class AudioMetadata {
   final String title;
   final String artists;
   final String mediaUrl;
   final String thumbnailUrl;
   final String videoId;
+  final bool? rating;
 
   AudioMetadata({
     required this.title,
@@ -13,63 +12,6 @@ class AudioMetadata {
     required this.mediaUrl,
     required this.thumbnailUrl,
     required this.videoId,
+    required this.rating,
   });
-}
-
-CancelToken cancelToken = CancelToken();
-Future<AudioMetadata?> getMedia({required String videoId}) async {
-  late Response response;
-  late Response getResponse;
-  String url = "http://10.0.2.2:8000/api/media";
-  bool connectionSuccessful = false;
-
-  Dio dio = Dio();
-  dio.options.contentType = 'application/json; charset=UTF-8';
-  dio.options.headers['Connection'] = 'Keep-Alive';
-  dio.options.headers["Accept"] = "application/json";
-
-  cancelToken.cancel();
-  cancelToken = CancelToken();
-  while (!connectionSuccessful) {
-    try {
-      response = await dio.post(
-        url,
-        data: {
-          'videoId': videoId,
-        },
-        options: Options(
-            followRedirects: true,
-            validateStatus: (status) {
-              return status! < 500;
-            }),
-        cancelToken: cancelToken,
-      );
-      String resphead = response.headers["location"]![0].toString();
-      getResponse = await dio.post(
-        resphead,
-        data: {
-          'videoId': videoId,
-        },
-        options: Options(
-            followRedirects: true,
-            validateStatus: (status) {
-              return status! < 500;
-            }),
-        cancelToken: cancelToken,
-      );
-      if (getResponse.statusCode == 200) {
-        connectionSuccessful = true;
-        Map<String, dynamic> data = getResponse.data;
-        String title = data["title"];
-        String artists = data["author"];
-        String mediaUrl = data["audioUrl"];
-        String thumbnailUrl = data["thumbnail"];
-        String videoId = data["videoId"];
-        return AudioMetadata(title: title, artists: artists, mediaUrl: mediaUrl, thumbnailUrl: thumbnailUrl, videoId: videoId);
-      }
-    } catch (e) {
-      // return Future.error(e.toString());
-    }
-  }
-  return null;
 }
