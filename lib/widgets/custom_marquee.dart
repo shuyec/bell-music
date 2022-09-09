226e67bb9fc37c45ae69fccec65b87335c26ec6c
Shu <76391491+shuyec@users.noreply.github.com>
2022-09-09 23:26:30 +0200

    Fixed marquee sizedbox

--- lib/widgets/custom_marquee.dart
+++ lib/widgets/custom_marquee.dart
@@ -28,68 +28,68 @@ class CustomMarquee extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return width == 0
-        ? SizedBox(
-            height: height,
-            child: willTextOverflow(
-              text: text,
-              style: style,
-            )
-                ? Marquee(
-                    key: UniqueKey(),
-                    text: text,
-                    style: style,
-                    scrollAxis: Axis.horizontal,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    blankSpace: 20.0,
-                    // velocity: 100.0,
-                    startAfter: const Duration(seconds: 1),
-                    pauseAfterRound: const Duration(seconds: 1),
-                    showFadingOnlyWhenScrolling: true,
-                    startPadding: 10.0,
-                    accelerationDuration: const Duration(seconds: 1),
-                    accelerationCurve: Curves.linear,
-                    decelerationDuration: const Duration(milliseconds: 500),
-                    decelerationCurve: Curves.easeOut,
-                  )
-                : SizedBox(
-                    height: height,
-                    child: Text(
-                      text,
-                      style: style,
-                    ),
-                  ),
+        ? willTextOverflow(
+            text: text,
+            style: style,
           )
-        : SizedBox(
-            height: height,
-            width: width,
-            child: willTextOverflow(
-              text: text,
-              style: style,
-              maxWidth: width,
-            )
-                ? Marquee(
-                    key: UniqueKey(),
-                    text: text,
-                    style: style,
-                    scrollAxis: Axis.horizontal,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    blankSpace: 20.0,
-                    // velocity: 100.0,
-                    startAfter: const Duration(seconds: 1),
-                    pauseAfterRound: const Duration(seconds: 1),
-                    showFadingOnlyWhenScrolling: true,
-                    accelerationDuration: const Duration(seconds: 1),
-                    accelerationCurve: Curves.linear,
-                    decelerationDuration: const Duration(milliseconds: 500),
-                    decelerationCurve: Curves.easeOut,
-                  )
-                : SizedBox(
-                    height: height,
-                    child: Text(
-                      text,
-                      style: style,
-                    ),
-                  ),
-          );
+            ? SizedBox(
+                height: height,
+                child: Marquee(
+                  key: UniqueKey(),
+                  text: text,
+                  style: style,
+                  scrollAxis: Axis.horizontal,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  blankSpace: 20.0,
+                  // velocity: 100.0,
+                  startAfter: const Duration(seconds: 1),
+                  pauseAfterRound: const Duration(seconds: 1),
+                  showFadingOnlyWhenScrolling: true,
+                  startPadding: 10.0,
+                  accelerationDuration: const Duration(seconds: 1),
+                  accelerationCurve: Curves.linear,
+                  decelerationDuration: const Duration(milliseconds: 500),
+                  decelerationCurve: Curves.easeOut,
+                ),
+              )
+            : SizedBox(
+                height: height,
+                child: Text(
+                  text,
+                  style: style,
+                ),
+              )
+        : willTextOverflow(
+            text: text,
+            style: style,
+            maxWidth: width,
+          )
+            ? SizedBox(
+                height: height,
+                width: width,
+                child: Marquee(
+                  key: UniqueKey(),
+                  text: text,
+                  style: style,
+                  scrollAxis: Axis.horizontal,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  blankSpace: 20.0,
+                  // velocity: 100.0,
+                  startAfter: const Duration(seconds: 1),
+                  pauseAfterRound: const Duration(seconds: 1),
+                  showFadingOnlyWhenScrolling: true,
+                  accelerationDuration: const Duration(seconds: 1),
+                  accelerationCurve: Curves.linear,
+                  decelerationDuration: const Duration(milliseconds: 500),
+                  decelerationCurve: Curves.easeOut,
+                ),
+              )
+            : SizedBox(
+                height: height,
+                child: Text(
+                  text,
+                  style: style,
+                ),
+              );
   }
 }
