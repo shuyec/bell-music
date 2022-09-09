75b85382cfcb42904f86a82653a5eaa36957f11c
Shu <76391491+shuyec@users.noreply.github.com>
2022-09-09 13:51:12 +0200

    Moved willTextOverflow function to here

--- lib/widgets/custom_marquee.dart
+++ lib/widgets/custom_marquee.dart
@@ -1,6 +1,6 @@
-import 'package:bell/general_functions.dart';
 import 'package:flutter/material.dart';
 import 'package:marquee/marquee.dart';
+import 'dart:ui';
 
 class CustomMarquee extends StatelessWidget {
   const CustomMarquee({Key? key, required this.text, required this.style, required this.height, this.width = 0}) : super(key: key);
@@ -9,6 +9,21 @@ class CustomMarquee extends StatelessWidget {
   final double width;
   final String text;
   final TextStyle style;
+
+  bool willTextOverflow({required String text, required TextStyle style, double maxWidth = 0}) {
+    if (maxWidth == 0) {
+      var pixelRatio = window.devicePixelRatio;
+      var logicalScreenSize = window.physicalSize / pixelRatio;
+      maxWidth = logicalScreenSize.width;
+    }
+    final TextPainter textPainter = TextPainter(
+      text: TextSpan(text: text, style: style),
+      maxLines: 1,
+      textDirection: TextDirection.ltr,
+    )..layout(minWidth: 0, maxWidth: maxWidth);
+
+    return textPainter.didExceedMaxLines;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +41,7 @@ class CustomMarquee extends StatelessWidget {
                     scrollAxis: Axis.horizontal,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     blankSpace: 20.0,
-                    velocity: 100.0,
+                    // velocity: 100.0,
                     startAfter: const Duration(seconds: 1),
                     pauseAfterRound: const Duration(seconds: 1),
                     showFadingOnlyWhenScrolling: true,
@@ -46,6 +61,7 @@ class CustomMarquee extends StatelessWidget {
           )
         : SizedBox(
             height: height,
+            width: width,
             child: willTextOverflow(
               text: text,
               style: style,
@@ -58,7 +74,7 @@ class CustomMarquee extends StatelessWidget {
                     scrollAxis: Axis.horizontal,
                     crossAxisAlignment: CrossAxisAlignment.center,
                     blankSpace: 20.0,
-                    velocity: 100.0,
+                    // velocity: 100.0,
                     startAfter: const Duration(seconds: 1),
                     pauseAfterRound: const Duration(seconds: 1),
                     showFadingOnlyWhenScrolling: true,
